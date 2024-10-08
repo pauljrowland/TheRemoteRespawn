8a36f26526c75cafd4f87f4df4f1bed8493f48e2
prowland@keywordsstudios.com <PaulJRowland@users.noreply.github.com>
2024-10-08 09:41:06 +0100

    202410.1 - Code tidied

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
 //    This script is deigned to be accessed via a CURL request, more instructions are
 //    below when visiting the page in a browser.
 
-$version = "202404.2";
+$version = "202410.1";
 $year = "2024";
 
 ?>
@@ -62,17 +62,17 @@ $year = "2024";
 //    Assemble variables to make the script run on any Pi with up to 8 Relay HATs.
 $ip = $_SERVER['SERVER_ADDR']; //Get the server IP for reference.
 $listBoards = "16relind -list"; //Command to count number of HATs attached.
-$numberOfHATs = shell_exec($listBoards); //Execute the command.
-if (str_contains($numberOfHATs, 'Failed to open the bus')) { //Error opening HATs.
+$numberOfHATs = shell_exec(command: $listBoards); //Execute the command.
+if (str_contains(haystack: $numberOfHATs, needle: 'Failed to open the bus')) { //Error opening HATs.
     echo "            <h2 style='color:red;'>\n";
     echo "                There has been an error communicating with the Relay HAT!<br>Please ensure it has been correctly installed and refer to the <a href='https://github.com/SequentMicrosystems/16relind-rpi' target='blank'>SequentMicrosystems GitHub</a> page for more information";
     echo "\n            </h2>\n";
     $numberOfHATs = 0; //Set number of HATs to 0, allowing the page to still display, alebit with the error.
 } else { //No issue communicating with the HATs.
-    $numberOfHATs = substr($numberOfHATs, 0, 1); //Rip out the number from the returned string.
+    $numberOfHATs = substr(string: $numberOfHATs, offset: 0, length: 1); //Rip out the number from the returned string.
 }
 $pcsSupported = $numberOfHATs * 8; //Times by 8 to get the max number of supported PCs.
-$hashedKeys = explode("\n", file_get_contents('../keys/auth_keys')); //Read auth_keys file to authenticate the user.
+$hashedKeys = explode(separator: "\n", string: file_get_contents(filename: '../keys/auth_keys')); //Read auth_keys file to authenticate the user.
 
 ?>
             <h1>Remote Respawn, v<?php echo $version; ?> - for Remote PCs</h1>
@@ -85,7 +85,7 @@ if($_POST) {
             $authError = TRUE;
             $errorText = "Please provide an API key to use this device"; //Tell the user to provide a key.
         }
-        if (!(in_array((hash('sha256', $_POST['apikey'])), $hashedKeys, true))){  //If confirmed it doesn't match - it must be wrong.
+        if (!(in_array(needle: (hash(algo: 'sha256', data: $_POST['apikey'])), haystack: $hashedKeys, strict: true))){  //If confirmed it doesn't match - it must be wrong.
             $authError = TRUE;
             $errorText = "The API key provided is invalid"; //Tell the user it's wrong.
         }
@@ -101,47 +101,47 @@ if($_POST) {
         $errorText = 'Invalid $computer:  &lt;NULL&gt;';
     }
     elseif (($computer < 1) or ($computer > $pcsSupported)) { //ERROR: Computer out of range.
-        $errorText = 'Invalid $computer (out of range (1-' .$pcsSupported. '):  ' .$computer;
+        $errorText = "Invalid \$computer (out of range (1-$pcsSupported):  $computer";
     }
     else {
-        $board = (intdiv(($computer - 1), 8)); //Take the PC number and use intdiv to work out which relay board it is attached to.
+        $board = intdiv(num1: $computer - 1, num2: 8); //Take the PC number and use intdiv to work out which relay board it is attached to.
         $relay = (($computer - 1) % 8) * 2 + 1; //Take the PC and use modulo to work out which relay number it is (1, 3, 5, 7, 9, 11, 13, 15).
     }
 
     if (empty($action)) { //ERROR: No action specified.
-        $errorText = 'Invalid $action:  &lt;NULL&gt;';
+        $errorText = "Invalid $action:  &lt;NULL&gt;";
     }
     elseif ($action == 'poweron') { //VALID: Power on the PC.
-        $piCommand = '16relind ' .$board. ' write '.$relay.' on; sleep 0.1; 16relind ' .$board. ' write '.$relay.' off';
+        $piCommand = "16relind $board write $relay on; sleep 0.1; 16relind $board write $relay off";
     }
     elseif ($action == 'shutdown') { //VALID: Shut down the PC.
-        $piCommand = '16relind ' .$board. ' write '.$relay.' on; sleep 0.1; 16relind ' .$board. ' write '.$relay.' off';
+        $piCommand = "16relind $board write $relay on; sleep 0.1; 16relind $board write $relay off";
     }
     elseif ($action == 'hardpoweroff') { //VALID: Hard power the PC off (hold the power button in for 5 seconds).
-        $piCommand = '16relind ' .$board. ' write '.$relay.' on; sleep 5; 16relind ' .$board. ' write '.$relay.' off';
+        $piCommand = "16relind $board write $relay on; sleep 5; 16relind $board write $relay off";
     }
     elseif ($action == 'hardreset') { //VALID: Hard reset the PC with the reset button (hold for 0.1 seconds).
         $relay = $relay + 1; //Add 1 to the relay number as the hardreset command is the next relay along.
-        $piCommand = '16relind ' .$board. ' write '.$relay.' on; sleep 0.1; 16relind ' .$board. ' write '.$relay.' off';
+        $piCommand = "16relind $board write $relay on; sleep 0.1; 16relind $board write $relay off";
     }
     else { //ERROR: Action specified was invalid.
-        $errorText = 'Invalid $action:  ' .$action;
+        $errorText = "Invalid \$action:  $action";
     }
 
     if (empty($errorText)) { //The $errorText variable has not been set - meaning there are no errors with the syntax of the URL specified.
-        $output = shell_exec($piCommand); //Execute the above action chosen.
-        $text = '<h1><span class="green">Success!</span></h1>Performed <strong>' .$action. '</strong> on PC <strong>' .$computer. '</strong> connected to board <strong>' .$board. '</strong> using relay <strong>' .$relay . '</strong><br><br>';
+        $output = shell_exec(command: $piCommand); //Execute the above action chosen.
+        $text = "<h1><span class=\"green\">Success!</span></h1>Performed <strong>$action</strong> on PC <strong>$computer</strong> connected to board <strong>$board</strong> using relay <strong>$relay</strong><br><br>";
         echo $text;
-        http_response_code(202); //Set the HTTP response code to 202 (Accepted).
+        http_response_code(response_code: 202); //Set the HTTP response code to 202 (Accepted).
     }
     elseif ($authError) {
-        http_response_code(401); //Set the HTTP response code to 401 (Unauthorized) to signal the key was incorrect or missing.
+        http_response_code(response_code: 401); //Set the HTTP response code to 401 (Unauthorized) to signal the key was incorrect or missing.
         $errorText = '<h1><span class="red">Failure!</span></h1>The following error was returned: <strong><span class="red">' .$errorText. '</span></strong>, Please try again.<br><br>';
         echo $errorText;
     }
     else { //ERROR: The $errorText variable had some text entered above, meaning there must be some sort of syntax error.
-        http_response_code(400); //Set the HTTP response code to 400 (Bad request) to signal to the sending app the URI was invalid.
-        $errorText = '<h1><span class="red">Failure!</span></h1>The following error was returned: <strong><span class="red">' .$errorText. '</span></strong>, Please try again.<br><br>';
+        http_response_code(response_code: 400); //Set the HTTP response code to 400 (Bad request) to signal to the sending app the URI was invalid.
+        $errorText = "<h1><span class=\"red\">Failure!</span></h1>The following error was returned: <strong><span class=\"red\">$errorText</span></strong>, Please try again.<br><br>";
         echo $errorText;
     }
 
