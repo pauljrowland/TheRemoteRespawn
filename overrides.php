661247e741c9190e7bd50edbc946c865496923ed
pauljrowland <17676570+pauljrowland@users.noreply.github.com>
2025-03-21 10:43:31 +0000

    Manual overrides

--- overrides.php
+++ overrides.php
@@ -1,6 +1,5 @@
 <?php
-    #Use this file to specify manuall oveerides (i.e. number of HATs in case of additional non-relay etc.)
-
+    #Use this file to specify manual overrides (i.e. number of HATs in case of additional non-relay etc.)
     # Example:
     ####   $forceNumberOfHATs = 6;
 ?>
